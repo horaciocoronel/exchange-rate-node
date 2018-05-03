f5c8e341c83755f9537733742f472b621f7a673d
horaciocoronel <arielc@gmail.com>
2018-05-03 15:04:32 -0400

    Create convertCurrency using async await

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const axios = require('axios');
 
-const getExchangeRate = (histDate, from, to) => {
-  axios.get(`https://exchangeratesapi.io/api/${histDate}?base=${from}&symbols=${to}`)
-    .then(res => {
-      let conversionCurr = to;
-      console.log(res.data.rates[to]);
-    })
-    .catch(err => {
-      console.log(err)
-    })
+const getExchangeRate = async(histDate, from, to) => {
+  try {
+    const response = await axios.get(`https://exchangeratesapi.io/api/${histDate}?base=${from}&symbols=${to}`)
+    const rate = response.data.rates[to];
+    if (rate) {
+      return rate
+    } else {
+      throw new Error(`Unable to get exchange rate for ${from} and ${to}`)
+    }
+    } catch (e) {
+      throw new Error(`Unable to get getExchangeRate for ${from} and ${to}`);
+    }
 }
 
-getExchangeRate('2016-06-05', 'USD', 'CAD');
+const convertCurrency = async(histDate, from, to, amount) => {
+  const rate = await getExchangeRate(histDate, from, to);
+  const exchangedAmount = amount * rate;
+  return(
+    {
+    "date": histDate,
+    "base_currency": from,
+    "base_amount": amount,
+    "conversion_currency": to,
+    "conversion_amount": exchangedAmount
+    })
+  
+}
+
+convertCurrency('2016-06-05', 'USD', 'CAD', 100)
+  .then((response) => {
+    response.send({response})
+  }
+  )
+  .catch(err => { console.log(err)})
