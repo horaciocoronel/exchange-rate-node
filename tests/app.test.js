b014d053a11eb7dc4a4961215c72e8b60248f7dd
horaciocoronel <arielc@gmail.com>
2018-05-07 16:45:21 -0400

    Change assertion library and functions. Add edge cases

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,4 +1,4 @@
-const expect = require('expect');
+const expect = require('chai').expect;
 const nock = require('nock');
 const { convertCurrency, getExchangeRate } = require('../app');
 
@@ -18,51 +18,65 @@ describe('Unit tests: convertCurrency - Nock', () => {
 		nock('https://exchangeratesapi.io')
 			.get('/api/2017-02-09?base=ZAR&symbols=TRY')
 			.reply(200, {"base":"ZAR","date":"2017-02-09","rates":{"TRY":0.2753940577}});
-
 	});
 
   it('Test case 1', async () => {
     const response = await convertCurrency('2011-06-03', 'USD', 100, 'CAD');   
-		expect(typeof response).toBe('object');			
-		expect(response.date).toBe('2011-06-03');	
-		expect(response.base_currency).toBe('USD');		
-		expect(typeof response.base_amount).toBe('number');		
-		expect(response.base_amount).toBe(100);		
-		expect(response.conversion_currency).toBe('CAD');		
-		expect(response.conversion_amount).toBe(97.85);
+		expect(typeof response).to.equal('object');			
+		expect(response.date).to.equal('2011-06-03');	
+		expect(response.base_currency).to.equal('USD');		
+		expect(typeof response.base_amount).to.equal('number');		
+		expect(response.base_amount).to.equal(100);		
+		expect(response.conversion_currency).to.equal('CAD');		
+		expect(response.conversion_amount).to.equal(97.85);
 	});
 	
   it('Test case 2', async () => {
     const response = await convertCurrency('2007-07-12', 'GBP', 303, 'SEK');   
-		expect(typeof response).toBe('object');			
-		expect(response.date).toBe('2007-07-12');	
-		expect(response.base_currency).toBe('GBP');		
-		expect(typeof response.base_amount).toBe('number');		
-		expect(response.base_amount).toBe(303);		
-		expect(response.conversion_currency).toBe('SEK');		
-		expect(response.conversion_amount).toBe(4085.0157);
+		expect(typeof response).to.equal('object');			
+		expect(response.date).to.equal('2007-07-12');	
+		expect(response.base_currency).to.equal('GBP');		
+		expect(typeof response.base_amount).to.equal('number');		
+		expect(response.base_amount).to.equal(303);		
+		expect(response.conversion_currency).to.equal('SEK');		
+		expect(response.conversion_amount).to.equal(4085.0157);
   });
 	
   it('Test case 3', async () => {
     const response = await convertCurrency('2004-08-07', 'EUR', 5, 'PLN');   
-		expect(typeof response).toBe('object');			
-		expect(response.date).toBe('2004-08-06');	
-		expect(response.base_currency).toBe('EUR');		
-		expect(typeof response.base_amount).toBe('number');		
-		expect(response.base_amount).toBe(5);		
-		expect(response.conversion_currency).toBe('PLN');		
-		expect(response.conversion_amount).toBe(22.01);
+		expect(typeof response).to.equal('object');			
+		expect(response.date).to.equal('2004-08-06');	
+		expect(response.base_currency).to.equal('EUR');		
+		expect(typeof response.base_amount).to.equal('number');		
+		expect(response.base_amount).to.equal(5);		
+		expect(response.conversion_currency).to.equal('PLN');		
+		expect(response.conversion_amount).to.equal(22.01);
   });
 	
   it('Test case 4', async () => {
     const response = await convertCurrency('2017-02-09', 'ZAR', 132, 'TRY');   
-		expect(typeof response).toBe('object');			
-		expect(response.date).toBe('2017-02-09');	
-		expect(response.base_currency).toBe('ZAR');		
-		expect(typeof response.base_amount).toBe('number');		
-		expect(response.base_amount).toBe(132);		
-		expect(response.conversion_currency).toBe('TRY');		
-		expect(response.conversion_amount).toBe(36.352);
+		expect(typeof response).to.equal('object');			
+		expect(response.date).to.equal('2017-02-09');	
+		expect(response.base_currency).to.equal('ZAR');		
+		expect(typeof response.base_amount).to.equal('number');		
+		expect(response.base_amount).to.equal(132);		
+		expect(response.conversion_currency).to.equal('TRY');		
+		expect(response.conversion_amount).to.equal(36.352);
   });
+});
+
+describe('Unit tests: edge cases convertCurrency - Nock', () => {
+	beforeEach(() => {		
+		nock('https://exchangeratesapi.io')
+			.get('/api/2011-06-03?base=USD&symbols=CAD')
+			.reply(200, {"base":"USD","date":"2011-06-03","rates":{"CAD":0.9785}});
+	});
+
+  it('Amount must be larger than zero', async () => {
+		const response = await convertCurrency('2011-06-03', 'USD', -3, 'CAD');
+		expect(
+			() => { throw response}
+		).to.throw('The amount to exchange must be larger than zero')
+	});
   
 });
